9de92e0025d4b2dec06398d4f4580621e4815c8a
Mikiyas Zenebe <mikilezen@gmail.com>
2026-08-04 18:16:20 +0300

    add model end point and update the erro handling

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, Request
 import joblib
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-model = joblib.load("spam_detector_model.pkl")
+model = joblib.load("naive_bayes_model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
-
 @app.post("/predict")
 async def predict(request: Request):
     try:
